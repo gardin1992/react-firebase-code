7677c67354e7ea073830902f0acec12992aa1016
Joao Gardin <gardin1992@gmail.com>
2021-04-03 14:07:31 -0300

    basic components

---
+++ src/scenes/NoMatch.js
@@ -0,0 +1,13 @@
+import { useLocation } from "react-router";
+
+export default function NoMatch() {
+  let location = useLocation();
+
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
+}
